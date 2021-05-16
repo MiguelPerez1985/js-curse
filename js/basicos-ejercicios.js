b08e1e6962a2bc91dd04a6251534495527ebf996
MiguelPerez1985 <miguel.1985@gmail.com>
2021-05-15 21:10:19 -0500

    :o

--- js/basicos-ejercicios.js
+++ js/basicos-ejercicios.js
@@ -163,7 +163,7 @@ isItPalindome("Eva, can I see bees in a cave?")
 
 // 8.- Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
 
-
+/*
 const deletePattern = function (text = "", pattern = "") {
 
     if (!text) return console.warn("The text is Empty"); 
@@ -177,3 +177,48 @@ const deletePattern = function (text = "", pattern = "") {
 }
 
 deletePattern("Programa una función que elimine cierto patrón de caracteres de un texto dado", "a, e, i, o, ó, u");
+*/
+
+
+/*
+9) Programa una función que obtenga un numero aleatorio entre 501 y 600.
+
+const randomNumber = () => console.log((Math.random() * (600-501)+501).toFixed()); 
+randomNumber();
+
+10) Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro), 
+pe. miFuncion(2002) devolverá true.
+
+const isItCapicua = function(number = 0) {
+    if(!number) return console.warn("Invalid Number");
+    let reversedNumber = parseInt(number.toString().split('').reverse().join(''));
+    (reversedNumber === number) 
+        ? console.log('It is Capicúa! :)')
+        : console.log('It isnt Capicúa :(');
+    
+}
+
+isItCapicua(4045);
+
+11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, 
+    se define como el producto de todos los números enteros positivos desde 1 hasta n), 
+    pe. miFuncion(5) devolverá 120.
+*/
+
+const factorial = function (num = 0) {
+    if(!num) return console.warn("Invalid number");
+    if(num<=0) return console.warn("U must type a number higher than 0");
+    let fact = 1;
+    for (let i = 1; i <= num; i++) {             
+        fact = fact * i;
+    }
+
+    return console.log(`El número factorial es ${fact}`); ;
+}
+
+factorial(8);
+
+
+
+
+
